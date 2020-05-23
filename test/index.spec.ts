359bfa96f730d5e2274555cf91d2fb9cb3298664
Jakub Biesiada <biesiadajakub@icloud.com>
2020-05-23 15:26:03 +0200

    test: rewrite tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,121 +1,99 @@
 import twoDigit from '../src';
 
-describe('one-digit number', () => {
-  const input = 5;
+describe('twoDigit', () => {
+  it('should return string with two-digit for one-digit number', () => {
+    const input = 5;
 
-  const value = twoDigit(input);
+    const value = twoDigit(input);
 
-  it('should return string with two-digit', () => {
     expect(value).toBe('05');
   });
-});
 
-describe('two-digit number', () => {
-  const input = 15;
+  it('should return number with two-digit for two-digit number', () => {
+    const input = 15;
 
-  const value = twoDigit(input);
+    const value = twoDigit(input);
 
-  it('should return number with two-digit', () => {
     expect(value).toBe(input);
   });
-});
-
-describe('floating point number', () => {
-  const input = 3.14;
-
-  const value = twoDigit(input);
 
   it('should return string with two-digit for floating point number', () => {
+    const input = 3.14;
+
+    const value = twoDigit(input);
+
     expect(value).toBe('03.14');
   });
-});
 
-describe('three-digit number', () => {
-  const input = 120;
+  it('should return inputted number with three-digit', () => {
+    const input = 120;
 
-  const value = twoDigit(input);
+    const value = twoDigit(input);
 
-  it('should return number with three-digit', () => {
     expect(value).toBe(input);
   });
-});
 
-describe('one-digit string', () => {
-  const input = '2';
+  it('should return string with two-digit for one-digit string', () => {
+    const input = '2';
 
-  const value = twoDigit(input);
+    const value = twoDigit(input);
 
-  it('should return string with two-digit', () => {
     expect(value).toBe('02');
   });
-});
 
-describe('two-digit string', () => {
-  const input = '10';
+  it('should return inputted string with two-digit', () => {
+    const input = '10';
 
-  const value = twoDigit(input);
+    const value = twoDigit(input);
 
-  it('should return string with two-digit', () => {
     expect(value).toBe(input);
   });
-});
 
-describe('three-digit string', () => {
-  const input = '204';
+  it('should return inputted string with three-digit', () => {
+    const input = '204';
 
-  const value = twoDigit(input);
+    const value = twoDigit(input);
 
-  it('should return string with three-digit', () => {
     expect(value).toBe(input);
   });
-});
 
-describe('negative number', () => {
-  const input = -1;
+  it('should return inputted negative number', () => {
+    const input = -1;
 
-  const value = twoDigit(input);
+    const value = twoDigit(input);
 
-  it('should return inputed value', () => {
     expect(value).toBe(input);
   });
-});
 
-describe('letter input', () => {
-  const input = 'J';
+  it('should return inputted letter', () => {
+    const input = 'J';
 
-  const value = twoDigit(input);
+    const value = twoDigit(input);
 
-  it('should return inputed value', () => {
     expect(value).toBe(input);
   });
-});
 
-describe('text input', () => {
-  const input = 'Hello World!';
+  it('should return inputted text', () => {
+    const input = 'Hello World!';
 
-  const value = twoDigit(input);
+    const value = twoDigit(input);
 
-  it('should return inputed value', () => {
     expect(value).toBe(input);
   });
-});
 
-describe('unsupported type', () => {
-  const input = { name: 'John' };
+  it('should return inputted unsupported type value', () => {
+    const input = { name: 'John' };
 
-  const value = twoDigit(input);
+    const value = twoDigit(input);
 
-  it('should return inputed value', () => {
     expect(JSON.stringify(value)).toBe(JSON.stringify(input));
   });
-});
 
-describe('Infinity value', () => {
-  const input = Infinity;
+  it('should return inputted Infinity value', () => {
+    const input = Infinity;
 
-  const value = twoDigit(input);
+    const value = twoDigit(input);
 
-  it('should return inputed value', () => {
     expect(value).toBe(input);
   });
 });
