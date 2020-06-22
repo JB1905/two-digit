607fbc18565d00bfa58178b68276dc37168b9b5c
Jakub Biesiada <biesiadajakub@icloud.com>
2020-06-22 17:28:13 +0200

    test: clean up tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,99 +1,23 @@
 import twoDigit from '../src';
 
 describe('twoDigit', () => {
-  it('should return string with two-digit for one-digit number', () => {
-    const input = 5;
-
+  it.each`
+    input               | expected
+    ${5}                | ${'05'}
+    ${15}               | ${15}
+    ${3.14}             | ${'03.14'}
+    ${120}              | ${120}
+    ${'2'}              | ${'02'}
+    ${'10'}             | ${'10'}
+    ${'204'}            | ${'204'}
+    ${-1}               | ${-1}
+    ${'J'}              | ${'J'}
+    ${'Hello World!'}   | ${'Hello World!'}
+    ${{ name: 'John' }} | ${{ name: 'John' }}
+    ${Infinity}         | ${Infinity}
+  `('sholud convert $input to $expected', ({ input, expected }) => {
     const value = twoDigit(input);
 
-    expect(value).toBe('05');
-  });
-
-  it('should return number with two-digit for two-digit number', () => {
-    const input = 15;
-
-    const value = twoDigit(input);
-
-    expect(value).toBe(input);
-  });
-
-  it('should return string with two-digit for floating point number', () => {
-    const input = 3.14;
-
-    const value = twoDigit(input);
-
-    expect(value).toBe('03.14');
-  });
-
-  it('should return inputted number with three-digit', () => {
-    const input = 120;
-
-    const value = twoDigit(input);
-
-    expect(value).toBe(input);
-  });
-
-  it('should return string with two-digit for one-digit string', () => {
-    const input = '2';
-
-    const value = twoDigit(input);
-
-    expect(value).toBe('02');
-  });
-
-  it('should return inputted string with two-digit', () => {
-    const input = '10';
-
-    const value = twoDigit(input);
-
-    expect(value).toBe(input);
-  });
-
-  it('should return inputted string with three-digit', () => {
-    const input = '204';
-
-    const value = twoDigit(input);
-
-    expect(value).toBe(input);
-  });
-
-  it('should return inputted negative number', () => {
-    const input = -1;
-
-    const value = twoDigit(input);
-
-    expect(value).toBe(input);
-  });
-
-  it('should return inputted letter', () => {
-    const input = 'J';
-
-    const value = twoDigit(input);
-
-    expect(value).toBe(input);
-  });
-
-  it('should return inputted text', () => {
-    const input = 'Hello World!';
-
-    const value = twoDigit(input);
-
-    expect(value).toBe(input);
-  });
-
-  it('should return inputted unsupported type value', () => {
-    const input = { name: 'John' };
-
-    const value = twoDigit(input);
-
-    expect(JSON.stringify(value)).toBe(JSON.stringify(input));
-  });
-
-  it('should return inputted Infinity value', () => {
-    const input = Infinity;
-
-    const value = twoDigit(input);
-
-    expect(value).toBe(input);
+    expect(value).toStrictEqual(expected);
   });
 });
