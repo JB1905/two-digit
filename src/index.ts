526081ecfc2decccae254abf2412373cedd1db68
Jakub Biesiada <biesiadajakub@icloud.com>
2020-09-27 09:43:55 +0200

    feat: return stringified value for number output

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import { checkString, checkNumber } from './helpers';
 
-function twoDigit(value: string): string;
-function twoDigit(value: number): number | string;
+function twoDigit(value: string | number): string;
 function twoDigit<T>(value: T): T;
 
 function twoDigit(value: any) {
   if (typeof value === 'string') {
     return checkString(value);
   } else if (typeof value === 'number') {
-    return checkNumber(value);
+    return `${checkNumber(value)}`;
   }
 
   return value;
